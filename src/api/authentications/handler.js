5a03915db9a789ca4074c8407584f28861f48a44
hanisy <pn232699@gmail.com>
2022-04-20 13:00:55 +0700

    authorization implemented

--- src/api/authentications/handler.js
+++ src/api/authentications/handler.js
@@ -42,7 +42,6 @@ class AuthenticationsHandler {
           message: error.message,
         });
         response.code(error.statusCode);
-        console.error(error);
         return response;
       }
 
